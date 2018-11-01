e3e8f07253fbe1f1f0f229b205f8c6c39fe4b291
Stephan van de Kruis <Stephan.van.de.Kruis@copaco.com>
2018-11-01 12:28:11 +0100

    first release

---
+++ Backup-recovery-2018/vswitch.ps1
@@ -0,0 +1,7 @@
+$ifindex = Get-NetAdapter -Name "vEthernet (SwitchName)"
+
+New-VMSwitch -SwitchName "SwitchName" -SwitchType Internal
+
+New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex $ifindex.ifIndex
+
+New-NetNat -Name MyNATnetwork -InternalIPInterfaceAddressPrefix 192.168.0.0/24
