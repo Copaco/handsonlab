9d16bb605369acdc2108d9a5f999ee4225326382
stephanvandekruis <41294872+stephanvandekruis@users.noreply.github.com>
2019-09-24 09:24:43 +0200

    Delete RoleAssignment.ps1

--- RoleAssignment.ps1
+++
@@ -1,3 +0,0 @@
-$FileShareContributorRole = Get-AzRoleDefinition "AFReadWriteRole"
-$scope = "/subscriptions/[subscriptionID]/resourceGroups/[resourcegroup]/providers/Microsoft.Storage/storageAccounts/[storageaccount]/fileServices/default/fileshare/[filesharename]"
-New-AzRoleAssignment -SignInName [UPN_Email] -RoleDefinitionName $FileShareContributorRole.Name -Scope $scope
